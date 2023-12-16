c9e668e58b560db629a4bc849b2b9265f155b57f
VladimirKovaliev <kovaliev.v@list.ru>
2023-12-16 20:22:40 +0300

    from FBV to CBV, new funcs

--- main/views.py
+++ main/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import render, get_object_or_404
-from main.models import Student
-from django.views.generic import ListView, DetailView
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy, reverse
 
+from main.models import Student
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 
 class StudentListView(ListView):
     model = Student
     template_name = 'main/material_list.html'
-
 
 
 def contact(request):
@@ -22,8 +22,35 @@ def contact(request):
     }
     return render(request, 'main/contact.html', context)
 
+
 class StudentDetailView(DetailView):
     model = Student
     template_name = 'main/student_detail.html'
 
 
+class StudentCreateView(CreateView):
+    model = Student
+    fields = ('first_name', 'last_name', 'avatar')
+    success_url = reverse_lazy('main:index')
+
+
+class StudentUpdateView(UpdateView):
+    model = Student
+    fields = ('first_name', 'last_name', 'avatar')
+    success_url = reverse_lazy('main:index')
+
+
+class StudentDeleteView(DeleteView):
+    model = Student
+    success_url = reverse_lazy('main:index')
+
+
+def toggle_activity(request, pk):
+    student_item = get_object_or_404(Student, pk=pk)
+    if student_item.is_active:
+        student_item.is_active = False
+    else:
+        student_item.is_active = True
+
+    student_item.save()
+    return redirect(reverse('main:index'))
